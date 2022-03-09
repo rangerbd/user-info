072876d54cf4829c77e3e43453341d48d8d3d287
rangerbd <dipto.g1@gmail.com>
2022-03-09 12:30:36 +0600

    adding header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import userdata from './data/data.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users ,setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(userdata);
+    console.log(userdata);
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+      
     </div>
   );
 }
